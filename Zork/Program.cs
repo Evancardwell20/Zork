f778b90e1305d07410196c4b87fefb29cd133218
Evan Cardwell <evancardwell@knights.ucf.edu>
2022-09-07 15:20:06 -0400

    Zork 2.1 Initial Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.ComponentModel.Design;
+using System.Linq;
 
 namespace Zork
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Zork!");
 
             bool isRunning = true;
             while (isRunning)
             {
-                Console.Write("> ");
+                Console.Write($"{_rooms[_currentRoom]} \n> ");
 
                 string inputString = Console.ReadLine().Trim();
                 Commands command = ToCommand(inputString);
@@ -33,17 +34,54 @@ namespace Zork
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
+                        if (Move(command))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+
+                        else
+                        {
+                            outputString = $"The way is shut."; 
+                        }
+                        
                         outputString = $"You moved {command}.";
                         break;
-                        
+
                     default:
-                        outputString = "Unknown Command"; 
+                        outputString = "Unknown Command";
                         break;
                 }
 
                 Console.WriteLine(outputString);
             }
         }
-        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN;
+
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST when _currentRoom < _rooms.Length - 1:
+                    _currentRoom++;
+                    didMove = true;
+                    break;
+
+                case Commands.WEST when _currentRoom > _rooms.Length - 1:
+                    _currentRoom--;
+                    didMove = true;
+                    break;
+            }
+            
+            return didMove; 
+        }
+
+        private static string[] _rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int _currentRoom = 1; 
     }
 }
