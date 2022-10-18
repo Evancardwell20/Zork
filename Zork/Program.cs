58d3ecfc91e3d9199aa99e6404a9c4e84a8b74d2
Evancardwell20 <86748377+Evancardwell20@users.noreply.github.com>
2022-10-17 22:07:51 -0400

    4.3 Initial Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,114 +4,23 @@ using Newtonsoft.Json;
 
 namespace Zork
 {
-    class Program
+    public class Program
     {
-        private static Room CurrentRoom
+        static void Main(string[] args)
         {
-            get
-            {
-                return _rooms[_location.Row, _location.Column];
-            }
-        }
-        static void Main()
-        {
-            string roomsFileName = "Rooms.json";
-            InitializeRoomDescription(roomsFileName);
-            Console.WriteLine("Welcome To Zork!");
+            const string defaultFileName = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultFileName);
 
-            Room previousRoom = null;
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork!");
 
-            bool isRunning = true;
-            while (isRunning)
-            {             
-                if (previousRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                }
-                Console.Write("> ");
-
-                string inputString = Console.ReadLine().Trim();
-                Commands command = ToCommand(inputString);
-
-                string outputString;
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        isRunning = false;
-                        outputString = "Thank you for playing!";
-                        break;
-
-                    case Commands.LOOK:
-                        outputString = CurrentRoom.Description;
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command))
-                        {
-                            outputString = $"You moved {command}.";
-                        }
-
-                        else
-                        {
-                            outputString = $"The way is shut."; 
-                        }
-                        break; 
-
-                    default:
-                        outputString = "Unknown Command";
-                        break;
-                }
-
-                Console.WriteLine(outputString);
-            }
-        }
-        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static bool Move(Commands command)
-        {
-            bool didMove = false;
-
-            switch (command)
-            {
-                case Commands.NORTH when _location.Row < _rooms.GetLength(0) - 1:
-                    _location.Row++;
-                    didMove = true;
-                    break;
-
-                case Commands.SOUTH when _location.Row > 0:
-                    _location.Row--;
-                    didMove = true;
-                    break;
-
-                case Commands.EAST when _location.Column < _rooms.GetLength(1) - 1:
-                    _location.Column++;
-                    didMove = true;
-                    break;
-
-                case Commands.WEST when _location.Column > 0:
-                    _location.Column--;
-                    didMove = true;
-                    break;
-            }
-            
-            return didMove; 
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
 
-        private static void InitializeRoomDescription(string roomsFileName) => 
-             _rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFileName));
-
-        private static Room[,] _rooms;
-
-        private static (int Row, int Column) _location = (1, 1); 
-
-        private enum Fields
+        private enum CommandLineArguments
         {
-            Name = 0,
-            Description
+            GameFilename = 0
         }
     }
 }
--- Zork/Room.cs
+++ Zork/Room.cs
@@ -1,15 +1,59 @@
-﻿namespace Zork
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Zork
 {
-    internal class Room
-    {
-        public string Name { get; set; }
+    public class Room
+    {   
+        [JsonProperty(Order = 1)]
+        public string Name { get; private set; }
         
+        [JsonProperty(Order = 2)]
         public string Description { get; set; }
 
-        public Room(string name, string description = null)
+        [JsonProperty(PropertyName = "Neighbors", Order = 3)]
+        private Dictionary<Directions, string> NeighborNames { get; set; } 
+
+        [JsonIgnore]
+        public IReadOnlyDictionary<Directions, Room> Neighbors { get; private set; }
+
+        public static bool operator ==(Room lhs, Room rhs)
         {
-            Name = name;
-            Description = description;
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return true;
+            }
+
+            if (lhs is null || rhs is null)
+            {
+                return false;
+            }
+
+            return lhs.Name == rhs.Name;
         }
+
+        public static bool operator !=(Room lhs, Room rhs) => !(lhs == rhs);
+
+        public override bool Equals(object obj) => obj is Room room ? this == room : false;
+
+        public bool Equals(Room other) => this == other;
+
+        public override string ToString() => Name;
+
+        public override int GetHashCode() => Name.GetHashCode();
+
+        public void UpdateNeighbors(World world) => Neighbors = (from entry in NeighborNames
+                                                                 let room = world.RoomsByName.GetValueOrDefault(entry.Value)
+                                                                 where room != null
+                                                                 select (Direction: entry.Key, Room: room))
+                                                                 .ToDictionary(pair => pair.Direction, pair => pair.Room); 
+
+        //public Room(string name, string description = null)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //}
     }
 }
